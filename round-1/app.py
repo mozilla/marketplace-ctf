255051449cccb5c75a245d6d10bb524cd10c7295
Andy McKay <amckay@mozilla.com>
2013-12-06 20:36:15 -0800

    first pass, too easy

---
+++ round-1/app.py
@@ -0,0 +1,51 @@
+import sqlite3
+from flask import Flask, redirect, render_template, request, session
+
+DATABASE = 'the.db'
+
+
+def get_db():
+    return sqlite3.connect(DATABASE)
+
+app = Flask(__name__)
+
+cmds = [
+    """CREATE TABLE IF NOT EXISTS users
+    (user varchar(255), password varchar(255), UNIQUE (user));""",
+    # You could login using the password below, but thats not the point.
+    "INSERT OR IGNORE INTO users (user, password) VALUES ('admin', 'awooga');"
+]
+
+db = get_db()
+for cmd in cmds:
+    db.cursor().execute(cmd)
+    db.commit()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('page.html', user=session.get('user'))
+
+
+@app.route('/', methods=['POST'])
+def login():
+    db = get_db()
+    sql = "SELECT user FROM users WHERE user = '%s' AND password = '%s'" % (
+        request.form['user'], request.form['password'])
+    res = db.cursor().execute(sql).fetchone()
+    if res:
+        session['user'] = res[0]
+
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    del session['user']
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'awldkahufkesufgaliefughawlefiughawefliaughefkajseyfgsldh'
+    app.debug = True
+    app.run()
