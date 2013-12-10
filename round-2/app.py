5dd04f5e8ad78f20cee4bca36f606882e159dbb1
Andy McKay <amckay@mozilla.com>
2013-12-09 20:52:02 -0800

    round 2

--- round-2/app.py
+++ round-2/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-import uuid
+import subprocess
+
 from flask import Flask, redirect, render_template, request, session
 
 DATABASE = 'the.db'
@@ -13,9 +14,7 @@ app = Flask(__name__)
 cmds = [
     """CREATE TABLE IF NOT EXISTS users
     (user varchar(255), password varchar(255), UNIQUE (user));""",
-    # You could login using the password below, but thats not the point.
-    "INSERT OR IGNORE INTO users (user, password) VALUES ('admin', '%s');" %
-    uuid.uuid4()
+    "INSERT OR IGNORE INTO users (user, password) VALUES ('admin', 'awooga');"
 ]
 
 db = get_db()
@@ -26,17 +25,24 @@ for cmd in cmds:
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('page.html', user=session.get('user'))
+    return render_template('page.html', session=session)
 
 
 @app.route('/', methods=['POST'])
 def login():
     db = get_db()
-    sql = "SELECT user FROM users WHERE user = '%s' AND password = '%s'" % (
-        request.form['user'], request.form['password'])
-    res = db.cursor().execute(sql).fetchone()
+    sql = "SELECT user FROM users WHERE user = ? AND password = ?"
+    user, password = request.form['user'], request.form['password']
+    res = (db.cursor().execute(sql, (user, password)).fetchone())
     if res:
         session['user'] = res[0]
+    else:
+        try:
+            cmd = 'cat hints/%s.txt' % user
+            session['hints'] = subprocess.check_output(cmd, shell=True)
+        except subprocess.CalledProcessError:
+            # Hint doesn't exist for the user, skip.
+            session['hints'] = ''
 
     return redirect('/')
 
@@ -48,6 +54,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.secret_key = 'round-1-baby'
+    app.secret_key = 'you-thought-round-1-was-bad?'
     app.debug = True
     app.run()
